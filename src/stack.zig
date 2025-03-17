8c85a9b46e30952516b83b6f358a5935483c1715
sadbeast <sadbeast@sadbeast.com>
2025-03-16 18:02:16 -0700

    Add Stack type to represent F18 stacks
    Both return and data stacks are backed by an 8 element stack. The data
    stack extends the stack by with two additional registers representing
    the top of the stack (t) and below the top (s). The return stack extends
    it's stack by one with the R register (t), and does not use (s). This
    does mean the return stack has an extra unused Word...possibly some
    comptime/reflection magic could remove it?

---
+++ src/stack.zig
@@ -0,0 +1,171 @@
+pub fn Stack(T: type, use_s: bool) type {
+    return struct {
+        t: T,
+        s: T,
+        stack: [8]T,
+        index: u3 = 0,
+
+        pub fn push(self: *@This(), value: T) void {
+            // std.debug.print("push\n", .{});
+            if (use_s) {
+                self.stack[self.index] = self.s;
+                self.s = self.t;
+            } else {
+                self.stack[self.index] = self.t;
+            }
+
+            self.t = value;
+            self.index +%= 1;
+        }
+
+        pub fn pop(self: *@This()) T {
+            self.index -%= 1;
+            // std.debug.print("pop\n", .{});
+            const old_t = self.t;
+            if (use_s) {
+                self.t = self.s;
+                self.s = self.stack[self.index];
+            } else {
+                self.t = self.stack[self.index];
+                // std.debug.print("ndex: {d}\n", .{self.index});
+            }
+
+            return old_t;
+        }
+
+        pub const empty: @This() = .{
+            .stack = @splat(0),
+            .index = 0,
+            .t = 0,
+            .s = 0,
+        };
+    };
+}
+pub const DataStack = Stack(Word, true);
+pub const ReturnStack = Stack(Word, false);
+
+test "DataStack works" {
+    var stack: DataStack = .empty;
+
+    // initalized to zeroes
+    try expectEqual(stack.index, 0);
+    try expectEqual(0, stack.t);
+    try expectEqual(0, stack.s);
+
+    // push
+    stack.push(55);
+    try expectEqual(55, stack.t);
+    try expectEqual(0, stack.s);
+    try expectEqual(1, stack.index);
+    try expectEqual(0, stack.stack[0]);
+
+    stack.push(123);
+    try expectEqual(123, stack.t);
+    try expectEqual(55, stack.s);
+    try expectEqual(2, stack.index);
+    try expectEqual(0, stack.stack[0]);
+
+    stack.push(999);
+    try expectEqual(999, stack.t);
+    try expectEqual(123, stack.s);
+    try expectEqual(3, stack.index);
+    try expectEqual(55, stack.stack[stack.index - 1]);
+
+    _ = stack.pop();
+    // pop
+    try expectEqual(123, stack.pop());
+    try expectEqual(55, stack.t);
+    try expectEqual(0, stack.s);
+
+    try expectEqual(55, stack.pop());
+    try expectEqual(0, stack.index);
+    try expectEqual(0, stack.t);
+    try expectEqual(0, stack.s);
+
+    inline for (0..10) |i| {
+        stack.push(i);
+    }
+    try expectEqual(2, stack.index);
+
+    try expectEqual(9, stack.pop());
+    try expectEqual(1, stack.index);
+
+    stack.push(123);
+    try expectEqual(2, stack.index);
+    try expectEqual(123, stack.pop());
+    try expectEqual(1, stack.index);
+
+    try expectEqual(8, stack.pop());
+
+    for (0..7) |_| {
+        _ = stack.pop();
+    }
+
+    try expectEqual(1, stack.index);
+    try expectEqual(0, stack.pop());
+}
+
+fn printStack(stack: DataStack) void {
+    std.debug.print("stack index: {d}\n", .{stack.index});
+    std.debug.print("stack t: {d}\n", .{stack.t});
+    std.debug.print("stack s: {d}\n", .{stack.s});
+    for (stack.stack, 0..) |s, i| {
+        if (stack.index == i) {
+            std.debug.print("-> ", .{});
+        } else {
+            std.debug.print("   ", .{});
+        }
+        std.debug.print("{d}\n", .{s});
+    }
+    std.debug.print("----\n\n", .{});
+}
+
+test ReturnStack {
+    var stack: ReturnStack = .empty;
+
+    // initalized to zeroes
+    try expectEqual(stack.index, 0);
+    try expectEqual(0, stack.t);
+    try expectEqual(0, stack.s);
+
+    // push
+    stack.push(55);
+    try expectEqual(55, stack.t);
+    try expectEqual(0, stack.s);
+    try expectEqual(1, stack.index);
+    try expectEqual(0, stack.stack[0]);
+
+    stack.push(123);
+    try expectEqual(123, stack.t);
+    try expectEqual(0, stack.s);
+    try expectEqual(2, stack.index);
+    try expectEqual(55, stack.stack[stack.index - 1]);
+
+    // pop
+    try expectEqual(123, stack.pop());
+    try expectEqual(0, stack.stack[stack.index - 1]);
+    try expectEqual(0, stack.s);
+
+    try expectEqual(55, stack.pop());
+    try expectEqual(0, stack.index);
+    try expectEqual(0, stack.t);
+    try expectEqual(0, stack.s);
+
+    // circular stack
+    stack.push(123);
+    stack.push(123);
+    for (0..31) |_| {
+        stack.push(0x15555);
+    }
+
+    for (0..31) |_| {
+        _ = stack.pop();
+    }
+    try expectEqual(0x15555, stack.pop());
+}
+
+const f18 = @import("f18.zig");
+const Word = f18.Word;
+
+const std = @import("std");
+const expectEqual = std.testing.expectEqual;
