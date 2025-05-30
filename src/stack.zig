a0243c79f825efc14f6067a3457bc934debe53a8
Kent Smith <kentsmith@gmail.com>
2025-05-29 21:46:01 -0700

    Use separate Return and Data stack structs
    Comptime generics made for fairly DRY code, but still has some runtime
    checks. I think it's OK to repeat yourself here for now.

--- src/stack.zig
+++ src/stack.zig
@@ -1,45 +1,60 @@
-pub fn Stack(T: type, use_s: bool) type {
-    return struct {
-        t: T,
-        s: T,
-        stack: [8]T,
-        index: u3 = 0,
+pub const DataStack = struct {
+    t: Word,
+    s: Word,
+    stack: [8]Word,
+    index: u3 = 0,
 
-        pub fn push(self: *@This(), value: T) void {
-            if (use_s) {
-                self.stack[self.index] = self.s;
-                self.s = self.t;
-            } else {
-                self.stack[self.index] = self.t;
-            }
+    pub fn push(self: *@This(), value: Word) void {
+        self.stack[self.index] = self.s;
+        self.s = self.t;
 
-            self.t = value;
-            self.index +%= 1;
-        }
+        self.t = value;
+        self.index +%= 1;
+    }
 
-        pub fn pop(self: *@This()) T {
-            self.index -%= 1;
-            const old_t = self.t;
-            if (use_s) {
-                self.t = self.s;
-                self.s = self.stack[self.index];
-            } else {
-                self.t = self.stack[self.index];
-            }
+    pub fn pop(self: *@This()) Word {
+        self.index -%= 1;
+        const old_t = self.t;
+        self.t = self.s;
+        self.s = self.stack[self.index];
 
-            return old_t;
-        }
+        return old_t;
+    }
 
-        pub const empty: @This() = .{
-            .stack = @splat(0),
-            .index = 0,
-            .t = 0,
-            .s = 0,
-        };
+    pub const empty: @This() = .{
+        .stack = @splat(0),
+        .index = 0,
+        .t = 0,
+        .s = 0,
     };
-}
-pub const DataStack = Stack(Word, true);
-pub const ReturnStack = Stack(Word, false);
+};
+
+pub const ReturnStack = struct {
+    t: Word,
+    stack: [8]Word,
+    index: u3 = 0,
+
+    pub fn push(self: *@This(), value: Word) void {
+        self.stack[self.index] = self.t;
+
+        self.t = value;
+        self.index +%= 1;
+    }
+
+    pub fn pop(self: *@This()) Word {
+        self.index -%= 1;
+        const old_t = self.t;
+        self.t = self.stack[self.index];
+
+        return old_t;
+    }
+
+    pub const empty: @This() = .{
+        .stack = @splat(0),
+        .index = 0,
+        .t = 0,
+    };
+};
 
 test "DataStack works" {
     var stack: DataStack = .empty;
@@ -68,8 +83,9 @@ test "DataStack works" {
     try expectEqual(3, stack.index);
     try expectEqual(55, stack.stack[stack.index - 1]);
 
-    _ = stack.pop();
     // pop
+    _ = stack.pop();
+
     try expectEqual(123, stack.pop());
     try expectEqual(55, stack.t);
     try expectEqual(0, stack.s);
@@ -108,30 +124,25 @@ test ReturnStack {
     // initalized to zeroes
     try expectEqual(stack.index, 0);
     try expectEqual(0, stack.t);
-    try expectEqual(0, stack.s);
 
     // push
     stack.push(55);
     try expectEqual(55, stack.t);
-    try expectEqual(0, stack.s);
     try expectEqual(1, stack.index);
     try expectEqual(0, stack.stack[0]);
 
     stack.push(123);
     try expectEqual(123, stack.t);
-    try expectEqual(0, stack.s);
     try expectEqual(2, stack.index);
     try expectEqual(55, stack.stack[stack.index - 1]);
 
     // pop
     try expectEqual(123, stack.pop());
     try expectEqual(0, stack.stack[stack.index - 1]);
-    try expectEqual(0, stack.s);
 
     try expectEqual(55, stack.pop());
     try expectEqual(0, stack.index);
     try expectEqual(0, stack.t);
-    try expectEqual(0, stack.s);
 
     // circular stack
     stack.push(123);
